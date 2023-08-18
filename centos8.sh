97d869b180c9dc121bfba22c7c179cd56acf87fa
Diệp Xuân Nam <130529888+namdx999@users.noreply.github.com>
2023-08-18 22:04:57 +0700

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -1,118 +1,121 @@
 #!/bin/sh
-
-# Hàm generate random string 
 random() {
-  tr </dev/urandom -dc A-Za-z0-9 | head -c5
-  echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
-# Mảng ký tự để generate IP 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-
-# Hàm generate IP IPv6 random
 gen64() {
-  ip64() {
-    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-  }
-
-  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)" 
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
-# Cài đặt 3proxy 
 install_3proxy() {
-echo "installing 3proxy"
-mkdir -p /3proxy
-cd /3proxy
-URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
-wget -qO- $URL | bsdtar -xvf-
-cd 3proxy-0.9.3
-make -f Makefile.Linux
-mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
-wget https://raw.githubusercontent.com/xlandgroup/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
-cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
-systemctl link /usr/lib/systemd/system/3proxy.service
-systemctl daemon-reload
-echo "* hard nofile 999999" >> /etc/security/limits.conf
-echo "* soft nofile 999999" >> /etc/security/limits.conf
-echo "net.ipv6.conf.enp1s0.proxy_ndp=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-sysctl -p
-systemctl stop firewalld
-systemctl disable firewalld
+    echo "installing 3proxy"
+    mkdir -p /3proxy
+    cd /3proxy
+    URL="https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-0.9.3
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    mv /3proxy/3proxy-0.9.3/bin/3proxy /usr/local/etc/3proxy/bin/
+    wget https://raw.githubusercontent.com/xlandgroup/ipv4-ipv6-proxy/master/scripts/3proxy.service-Centos8 --output-document=/3proxy/3proxy-0.9.3/scripts/3proxy.service2
+    cp /3proxy/3proxy-0.9.3/scripts/3proxy.service2 /usr/lib/systemd/system/3proxy.service
+    systemctl link /usr/lib/systemd/system/3proxy.service
+    systemctl daemon-reload
+#    systemctl enable 3proxy
+    echo "* hard nofile 999999" >>  /etc/security/limits.conf
+    echo "* soft nofile 999999" >>  /etc/security/limits.conf
+    echo "net.ipv6.conf.enp1s0.proxy_ndp=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
+    sysctl -p
+    systemctl stop firewalld
+    systemctl disable firewalld
 
-cd $WORKDIR
+    cd $WORKDIR
 }
 
-# Tạo cấu hình 3proxy
 gen_3proxy() {
-
-  cat <<EOF
-  daemon
-  ...
-  
-  $(awk -F "/" '{print "allow" \\  
-             "proxy -6 -n -a -p" $2 " -i" $1 "\\n" \\
-             "flush\\n"}' ${WORKDATA})
+    cat <<EOF
+daemon
+maxconn 2000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+stacksize 6291456 
+flush
+auth strong
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
 EOF
-
-}  
-
-# Tạo dữ liệu proxy
-gen_data() {
-
-  seq $FIRST_PORT $LAST_PORT | while read port; do
-    echo "$IP6/$port/" >> $WORKDIR/data.txt
-  done
-
 }
 
-# Các hàm khác 
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
 
-WORKDIR=/root/proxy
+upload_proxy() {
+    cd $WORKDIR
+    local PASS=$("")
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
-# Khởi tạo biến 
-IP6=$(curl -6 icanhazip.com | cut -f1-4 -d':')
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
 
-# Chạy các bước tiếp theo
-
-gen_data
-gen_3proxy
-
-# Các bước còn lại
+}
 gen_data() {
-seq $FIRST_PORT LAST 
-PORT∣whilereadport;doecho"(random)/$(random)/$IP4/port/(gen64 $IP6)"
-done
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
 }
 
 gen_iptables() {
-cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
 gen_ifconfig() {
-cat <<EOF
+    cat <<EOF
 $(awk -F "/" '{print "ifconfig enp1s0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 echo "installing apps"
 yum -y install gcc net-tools bsdtar zip make >/dev/null
+
 install_3proxy
+
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd  
-IP4=(curl -4 -s icanhazip.com)
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = IP6"echo"Howmanyproxydoyouwanttocreate?Example500"readCOUNTFIRST 
-PORT=10000LAST 
-PORT=(($FIRST_PORT + $COUNT))
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
+echo "How many proxy do you want to create? Example 500"	
+read COUNT	
+FIRST_PORT=10000	
+LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
