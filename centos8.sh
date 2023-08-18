d392e23aa94e5640ffc2e39cd90d28d4d3425a7b
Diệp Xuân Nam <130529888+namdx999@users.noreply.github.com>
2023-08-18 11:34:42 +0700

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -1,55 +1,115 @@
 #!/bin/sh
 
-# Get the IP addresses and ports
-IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+# Hàm generate random string 
+random() {
+  tr </dev/urandom -dc A-Za-z0-9 | head -c5
+  echo
+}
 
-# Create the data file
-echo "" > data.txt
-for port in $(seq $FIRST_PORT $LAST_PORT); do
-    echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)" >> data.txt
+# Mảng ký tự để generate IP 
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+# Hàm generate IP IPv6 random
+gen64() {
+  ip64() {
+    echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+  }
+
+  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)" 
+}
+
+# Cài đặt 3proxy 
+install_3proxy() {
+  
+  # Các bước cài đặt giữ nguyên
+  
+}
+
+# Tạo cấu hình 3proxy
+gen_3proxy() {
+
+  cat <<EOF
+  daemon
+  ...
+  
+  $(awk -F "/" '{print "allow" \\  
+             "proxy -6 -n -a -p" $2 " -i" $1 "\\n" \\
+             "flush\\n"}' ${WORKDATA})
+EOF
+
+}  
+
+# Tạo dữ liệu proxy
+gen_data() {
+
+  seq $FIRST_PORT $LAST_PORT | while read port; do
+    echo "$IP6/$port/" >> $WORKDIR/data.txt
+  done
+
+}
+
+# Các hàm khác 
+
+WORKDIR=/root/proxy
+
+# Khởi tạo biến 
+IP6=$(curl -6 icanhazip.com | cut -f1-4 -d':')
+
+# Chạy các bước tiếp theo
+
+gen_data
+gen_3proxy
+
+# Các bước còn lại
+gen_data() {
+seq $FIRST_PORT LAST 
+PORT∣whilereadport;doecho"(random)/$(random)/$IP4/port/(gen64 $IP6)"
 done
+}
 
-# Install the necessary packages
-yum -y install gcc net-tools bsdtar zip make
-
-# Generate the configuration file for 3proxy
-cat <<EOF > 3proxy.cfg
-daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6291456
-flush
-auth strong
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' data.txt)
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' data.txt)
+gen_iptables() {
+cat <<EOF
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
+}
 
-# Start the 3proxy server
+gen_ifconfig() {
+cat <<EOF
+$(awk -F "/" '{print "ifconfig enp1s0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip make >/dev/null
+install_3proxy
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd  
+IP4=(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = IP6"echo"Howmanyproxydoyouwanttocreate?Example500"readCOUNTFIRST 
+PORT=10000LAST 
+PORT=(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x $WORKDIR/boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+systemctl start NetworkManager.service
+ifup enp1s0
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 65535
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
-
-# Create the proxy user file
-cat <<EOF > proxy.txt
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' data.txt)
 EOF
 
-# Upload the proxy file
-zip --password $PASS proxy.zip proxy.txt
-URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+bash /etc/rc.local
 
-# Notify the user
-echo "Proxy is ready! Format IP:PORT"
-echo "Download zip archive from: ${URL}"
-echo "Password: ${PASS}"
+gen_proxy_file_for_user
+
+upload_proxy
